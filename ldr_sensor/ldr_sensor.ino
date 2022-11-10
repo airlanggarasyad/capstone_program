e3d85dd1232065c86c9864dc90c026b9e416cb5f
Airlangga Fidiyanto <airlangga.rasyad@live.com>
2022-11-10 14:26:36 +0700

    add wifi functionality

--- ldr_sensor/ldr_sensor.ino
+++ ldr_sensor/ldr_sensor.ino
@@ -11,8 +11,87 @@ const char* mqtt_server = "broker.emqx.io";
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+double lightIntensity = 0;
+double averageLightIntensity = 0;
+
+String messageData;
+
 void setup() {
   Serial.begin(115200);
+
+  pinMode(ledPin, OUTPUT);
+  
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+  
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+
+  // Feel free to add more if statements to control more GPIOs with MQTT
+
+  // If a message is received on the topic esp32/output, you check if the message is either "on" or "off". 
+  // Changes the output state according to the message
+  if (String(topic) == "esp32/output") {
+    Serial.print("Changing output to ");
+    if(messageTemp == "on"){
+      Serial.println("on");
+      digitalWrite(ledPin, HIGH);
+    }
+    else if(messageTemp == "off"){
+      Serial.println("off");
+      digitalWrite(ledPin, LOW);
+    }
+  }
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+      // Subscribe
+      client.subscribe("esp32/output");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
 }
 
 double readLDRData() {
@@ -21,7 +100,13 @@ double readLDRData() {
   
   return (0.1784 * ldrRawData) + 93.93325;
 }
-
 void loop() {
- readLDRData();
+  for (int i=0; i<5; i++) {
+    lightIntensity = lightIntensity + readLDRData();
+  }
+
+  averageLightIntensity = lightIntensity / 5;
+  messageData = String(averageLightIntensity, 4);
+  
+  client.publish("capstoneA16/lightIntensity", messageData);
 }
