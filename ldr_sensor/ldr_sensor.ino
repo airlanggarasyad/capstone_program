8c107d17116d49edd28d684ff32401bc1c573af1
arifahamelia <90139251+arifahamelia@users.noreply.github.com>
2022-11-13 18:44:22 +0700

    updated ldr_sensor

--- ldr_sensor/ldr_sensor.ino
+++ ldr_sensor/ldr_sensor.ino
@@ -11,10 +11,11 @@ const char* mqtt_server = "broker.emqx.io";
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+int ledPin = 0;
 double lightIntensity = 0;
 double averageLightIntensity = 0;
 
-String messageData;
+char messageData[20];
 
 void setup() {
   Serial.begin(115200);
@@ -33,7 +34,7 @@ void setup_wifi() {
   Serial.print("Connecting to ");
   Serial.println(ssid);
 
-  WiFi.begin(ssid, password);
+  WiFi.begin(ssid, pass);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -62,17 +63,6 @@ void callback(char* topic, byte* message, unsigned int length) {
 
   // If a message is received on the topic esp32/output, you check if the message is either "on" or "off". 
   // Changes the output state according to the message
-  if (String(topic) == "esp32/output") {
-    Serial.print("Changing output to ");
-    if(messageTemp == "on"){
-      Serial.println("on");
-      digitalWrite(ledPin, HIGH);
-    }
-    else if(messageTemp == "off"){
-      Serial.println("off");
-      digitalWrite(ledPin, LOW);
-    }
-  }
 }
 
 void reconnect() {
@@ -98,15 +88,21 @@ double readLDRData() {
   int ldrRawData = 4095 - analogRead(LDR_PIN);
   delay(1000);
   
-  return (0.1784 * ldrRawData) + 93.93325;
+  return (0.28661 * ldrRawData) - 201.27866;
 }
 void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+
+  client.loop();
+  
   for (int i=0; i<5; i++) {
     lightIntensity = lightIntensity + readLDRData();
   }
 
   averageLightIntensity = lightIntensity / 5;
-  messageData = String(averageLightIntensity, 4);
-  
+//  messageData = String(averageLightIntensity, 4);
+  sprintf(messageData, "%f", averageLightIntensity);
   client.publish("capstoneA16/lightIntensity", messageData);
 }
