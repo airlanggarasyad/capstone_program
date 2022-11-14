8855e520f59475c01d673a9718cda575befbc60c
Airlangga Fidiyanto <airlangga.rasyad@live.com>
2022-11-14 15:18:24 +0700

    same adjustment for ldr program

--- ldr_sensor/ldr_sensor.ino
+++ ldr_sensor/ldr_sensor.ino
@@ -1,12 +1,12 @@
-#include <WiFi.h>
 #include <PubSubClient.h>
+#include <WiFi.h>
 
-#define LDR_PIN 36 //LDR pada pin A0
+#define LDR_PIN 36  // LDR pada pin A0
 
-const char* ssid = "REPLACE_WITH_SSID";
-const char* pass = "REPLACE_WITH_PASS";
+const char* ssid = "suastuti_3";
+const char* pass = "notaristutiek";
 
-const char* mqtt_server = "broker.emqx.io";
+const char* mqtt_server = "192.168.1.4";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -14,13 +14,15 @@ PubSubClient client(espClient);
 double lightIntensity = 0;
 double averageLightIntensity = 0;
 
+bool wifiIndState = false;
+
 String messageData;
 
 void setup() {
   Serial.begin(115200);
 
   pinMode(ledPin, OUTPUT);
-  
+
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
@@ -36,10 +38,13 @@ void setup_wifi() {
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
     Serial.print(".");
+    wifiIndState = !wifiIndState;
+    digitalWrite(LED_BUILTIN, wifiIndState);
+    delay(100);
   }
 
+  digitalWrite(LED_BUILTIN, HIGH);
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
@@ -51,28 +56,12 @@ void callback(char* topic, byte* message, unsigned int length) {
   Serial.print(topic);
   Serial.print(". Message: ");
   String messageTemp;
-  
+
   for (int i = 0; i < length; i++) {
     Serial.print((char)message[i]);
     messageTemp += (char)message[i];
   }
   Serial.println();
-
-  // Feel free to add more if statements to control more GPIOs with MQTT
-
-  // If a message is received on the topic esp32/output, you check if the message is either "on" or "off". 
-  // Changes the output state according to the message
-  if (String(topic) == "esp32/output") {
-    Serial.print("Changing output to ");
-    if(messageTemp == "on"){
-      Serial.println("on");
-      digitalWrite(ledPin, HIGH);
-    }
-    else if(messageTemp == "off"){
-      Serial.println("off");
-      digitalWrite(ledPin, LOW);
-    }
-  }
 }
 
 void reconnect() {
@@ -82,8 +71,6 @@ void reconnect() {
     // Attempt to connect
     if (client.connect("ESP8266Client")) {
       Serial.println("connected");
-      // Subscribe
-      client.subscribe("esp32/output");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -97,16 +84,16 @@ void reconnect() {
 double readLDRData() {
   int ldrRawData = 4095 - analogRead(LDR_PIN);
   delay(1000);
-  
+
   return (0.1784 * ldrRawData) + 93.93325;
 }
 void loop() {
-  for (int i=0; i<5; i++) {
+  for (int i = 0; i < 5; i++) {
     lightIntensity = lightIntensity + readLDRData();
   }
 
   averageLightIntensity = lightIntensity / 5;
   messageData = String(averageLightIntensity, 4);
-  
+
   client.publish("capstoneA16/lightIntensity", messageData);
 }
